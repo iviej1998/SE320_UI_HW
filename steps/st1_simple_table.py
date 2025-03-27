1e9c1fe8daa33f0aa12a86ca27140aea090c21b8
Jillian Ivie <iviej@my.erau.edu>
2025-03-27 11:13:39 -0700

    Add steps folder

---
+++ steps/st1_simple_table.py
@@ -0,0 +1,9 @@
+""" Streamlit front-end for api.weather.gov """
+import streamlit as st
+from requests import get
+
+
+forecast = get(
+    "https://api.weather.gov/points/36.4216,-109.4842", timeout=3).json()
+st.title("Weather Forecast")
+st.table(forecast.get("properties").get("periods"))
